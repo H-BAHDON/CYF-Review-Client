fe8293b552241b01413220199c9b89cfa16adbf5
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-09-16 12:39:30 +0100

    Created card Componets
    Co-Authored-By: Yulia Hospodar <122671093+yuliya110692@users.noreply.github.com>
    Co-Authored-By: olha25 <122283736+olha25@users.noreply.github.com>
    Co-Authored-By: shadi fakhri <113238379+shadi38@users.noreply.github.com>

---
+++ src/componets/cards/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CountLike from "../countLikes/CountLikes";
+const Card = (props) => {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h3 className="card-title">{props.title}</h3>
+        <p className="card-title">{props.lang}</p>
+        <p className="card-text">{props.description}</p>
+      </div>
+      <CountLike />
+    </div>
+  );
+};
+
+export default Card;
