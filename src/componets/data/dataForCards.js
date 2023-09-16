f50ff7fe050396d81035c215508af4008e2280b8
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-09-16 12:40:01 +0100

    Created Data for the Cards
    Co-Authored-By: Yulia Hospodar <122671093+yuliya110692@users.noreply.github.com>
    Co-Authored-By: olha25 <122283736+olha25@users.noreply.github.com>
    Co-Authored-By: shadi fakhri <113238379+shadi38@users.noreply.github.com>

---
+++ src/componets/data/dataForCards.js
@@ -0,0 +1,38 @@
+export const DataForCardView = [
+    {
+        id: 1,
+        title: "JavaScript Basics",
+        lang: "JavaScript",
+        description: "JavaScript is a versatile programming language used for web development. Learn the fundamentals of JavaScript, including variables, functions, and control structures."
+    },
+    {
+        id: 2,
+        title: "React.js Fundamentals",
+        lang: "JavaScript",
+        description: "React.js is a popular JavaScript library for building user interfaces. Explore the core concepts of React, such as components and props."
+    },
+    {
+        id: 3,
+        title: "Node.js Server Development",
+        lang: "JavaScript",
+        description: "Node.js allows you to build server-side applications using JavaScript. Discover how to create a server, handle HTTP requests, and work with databases."
+    },
+    {
+        id: 4,
+        title: "ES6 Features",
+        lang: "JavaScript",
+        description: "ECMAScript 6 (ES6) introduced many new features to JavaScript. Learn about arrow functions, classes, modules, and other ES6 enhancements."
+    },
+    {
+        id: 5,
+        title: "JavaScript Frameworks",
+        lang: "JavaScript",
+        description: "Explore popular JavaScript frameworks like Angular, Vue.js, and Ember.js. Understand their core concepts and when to use them in your projects."
+    },
+    {
+        id: 6,
+        title: "JavaScript Promises",
+        lang: "JavaScript",
+        description: "Promises are a powerful tool for handling asynchronous operations in JavaScript. Master the use of promises for cleaner and more efficient code."
+    }
+];
