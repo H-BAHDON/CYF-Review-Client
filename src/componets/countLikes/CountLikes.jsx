523af20e6597758415eaac716c45873ffdcb249e
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-09-16 12:39:46 +0100

    Created countlike Componets
    Co-Authored-By: Yulia Hospodar <122671093+yuliya110692@users.noreply.github.com>
    Co-Authored-By: olha25 <122283736+olha25@users.noreply.github.com>
    Co-Authored-By: shadi fakhri <113238379+shadi38@users.noreply.github.com>

---
+++ src/componets/countLikes/CountLikes.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+// import "./CountLike.css"
+
+const CountLikes = () => {
+  const [countUp, setCountUp] = useState(0);
+  const [countDown, setCountDown] = useState(0);
+
+  const handleCounterUp = () => {
+    setCountUp(countUp + 1);
+  };
+  const handleCounterDown = () => {
+    setCountDown(countDown - 1);
+  };
+  return (
+    <div>
+      <button className="up" onClick={handleCounterUp}>
+        <img
+          alt="tumb-up_picture"
+          className="tumb-up"
+          src="https://icon-library.com/images/white-thumbs-up-icon/white-thumbs-up-icon-26.jpg"
+        ></img>
+      </button>
+      <button className="down" onClick={handleCounterDown}>
+        <img
+          alt="tumb-down_picture"
+          className="tumb-down"
+          src="https://icon-library.com/images/white-thumbs-up-icon/white-thumbs-up-icon-26.jpg"
+        ></img>
+        <span></span>
+      </button>
+    </div>
+  );
+};
+
+export default CountLikes;
